f1917ed9e53a6490ef940651ef24b2dc6286c321
sydney miranda <sydmiranda@icloud.com>
2020-10-29 14:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
       var result = []
   for (var i = 0; i < musicians.length; i++) {
-    result.push(musicians[i]+' plays ' + instruments[i])
+    result.push(musicians[i] +' plays ' + instruments[i])
   }
   return result
 }
